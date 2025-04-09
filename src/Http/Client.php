9a7a452e33df076f976cfe3304f23daa175d39dc
emefye <47487664+emefye@users.noreply.github.com>
2025-04-09 16:42:52 +0700

    Update Client.php
    Make CURLOPT_SSL_VERIFYPEER a dynamic variable for improved SSL configuration flexibility.

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -234,7 +234,7 @@ class Client
         curl_setopt($curlHandle, CURLOPT_USERAGENT, 'Laravel/Flare API 1.0');
         curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curlHandle, CURLOPT_TIMEOUT, $this->timeout);
-        curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, $this->curlSslVerifyPeer);
         curl_setopt($curlHandle, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
         curl_setopt($curlHandle, CURLOPT_ENCODING, '');
         curl_setopt($curlHandle, CURLINFO_HEADER_OUT, true);
